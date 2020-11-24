0aff112273b5d439090262382c7b64bf66747dbb
chadkh <chadkh@uci.edu>
2020-11-24 15:41:36 -0800

    created functions to convert data into json objects

--- automate.py
+++ automate.py
@@ -1,3 +1,4 @@
+import json
 
 element = {'1':"Electro",
            '2':"Hydro",
@@ -12,49 +13,67 @@ weaponType = {'1':"Sword",
            '4':"Bow",
            '5':"Polearm",
            }
+    #create a list of dicts
+    #to convert to json 
 def makeCharList():
-    toRead = open('genshindata.txt',mode='r')
-    fullStr = ""
+    toRead = open('.\\data\\characterdata',mode='r')
+    # fullStr = ""
     charId = 0
+    char_list = []
     for x in toRead:
         name,code = x.rstrip().split(',')
         count = 0
-        string = ""
+        char_info = {} #dict to hold character information
         charId += 1
+        char_info["name"] = name
         for number in code:
             if count == 0:
-                string += f" rating : {number}\n id : {str(charId) + '1' + code}\n "
+                # string += f" rating : {number}\n id : {str(charId) + '1' + code}\n "
+                char_info["rating"] = number
+                char_info["id"] = str(charId) + "1" + code
             elif count == 1:
-                string += f"vision : {element[number]}\n "
+                # string += f"vision : {element[number]}\n "
+                char_info["vision"] = element[number]
             elif count == 2:
-                string += f"weapon : {weaponType[number]}\n"
+                # string += f"weapon : {weaponType[number]}\n"
+                char_info["weapon"] = weaponType[number]
             count += 1
 
-        name += f"\n{string}"
-        fullStr += name
+        # name += f"\n{string}"
+        # fullStr += name
+        char_list.append(char_info) #add the chardict into the list
+    # print(char_list)
+    with open(".\\data\\wanderLustInvocationDrops.json",'w') as outfile:
+        json.dump(char_list,outfile)
     
     toRead.close()
-    return fullStr
+    # return fullStr
     
-print(makeCharList())
+
 
 def makeWeaponList():
-    toRead = open('genshinweapondata.txt',mode='r')
+    toRead = open('.\\data\\weapondata',mode='r')
     fullStr = ""
     currentId = ""
-    
+    weapon_list = []
     for x in toRead:
         string = ""
         x = x.rstrip()
-        
+        weapon_data = {}
         if len(x) == 1:
             currentId = x
         else:
             name,wepType = x.split(',')
-            
-            string += f"name : {name}\n rating : {currentId}\n weaponType : {weaponType[wepType]}\n"
-        
-        fullStr += string
+            weapon_data["name"] = name
+            weapon_data["rating"] = currentId
+            weapon_data["weaponType"] = weaponType[wepType]
+            # string += f"name : {name}\n rating : {currentId}\n weaponType : {weaponType[wepType]}\n"
+            weapon_list.append(weapon_data)
+        # fullStr += string
+    with open(".\\data\\wanderLustInvocationDrops.json","a") as outfile:
+        json.dump(weapon_list,outfile)
+    # return fullStr
 
-    return fullStr
-print(makeWeaponList())
+
+makeCharList()
+makeWeaponList()
